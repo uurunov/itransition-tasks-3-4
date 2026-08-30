18b8164c50717cd6665c15c5e78002136a7d912c
Ulugbek Urunov <uurunov.dev@gmail.com>
2026-08-30 14:28:24 +0500

    lazily loaded route components

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,21 +1,33 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Dashboard } from './pages/dashboard/dashboard';
-import { Signup } from './pages/signup/signup';
 import { guestGuard } from './guards/guest-guard';
 import { userGuard } from './guards/user-guard';
-import { EmailConfirm } from './pages/email-confirmation/email-confirm';
 
 export const routes: Routes = [
-  { path: 'login', component: Login, canActivate: [guestGuard], title: 'Sign in' },
-  { path: 'register', component: Signup, canActivate: [guestGuard], title: 'Sign up' },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login').then((page) => page.Login),
+    canActivate: [guestGuard],
+    title: 'Sign in',
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/signup/signup').then((page) => page.Signup),
+    canActivate: [guestGuard],
+    title: 'Sign up',
+  },
   {
     path: 'email-confirmation',
-    component: EmailConfirm,
+    loadComponent: () =>
+      import('./pages/email-confirmation/email-confirm').then((page) => page.EmailConfirm),
     canActivate: [guestGuard],
     title: 'Confirm your email',
   },
-  { path: 'dashboard', component: Dashboard, canActivate: [userGuard], title: 'Dashboard' },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard').then((page) => page.Dashboard),
+    canActivate: [userGuard],
+    title: 'Dashboard',
+  },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
 ];
